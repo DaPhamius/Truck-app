79ca0805b4b5777348b33e8ff66a71fb86da6c51
Dan Pham <43536392+DaPhamius@users.noreply.github.com>
2020-02-14 20:20:36 +0100

    Updates
    Added Building. Missing Colliders

--- Truck App/Assets/Scripts/CarSpawner.cs
+++ Truck App/Assets/Scripts/CarSpawner.cs
@@ -9,6 +9,7 @@ public class CarSpawner : MonoBehaviour
     public GameObject placementIndicator;
     public GameObject carControls;
     public GameObject placeCarButton;
+    public GameObject building;
 
     private void Start()
     {
@@ -16,15 +17,18 @@ public class CarSpawner : MonoBehaviour
         carControls.SetActive(false);
         placeCarButton.SetActive(true);
         placementIndicator.SetActive(true);
+        building.SetActive(false);
     }
 
     public void OnPlaceCarButton()
     {
         car.SetActive(true);
         car.transform.position = placementIndicator.transform.position;
-
+        building.SetActive(true);
+        building.transform.position = placementIndicator.transform.position;
         carControls.SetActive(true);
         placeCarButton.SetActive(false);
         placementIndicator.SetActive(false);
+
     }
 }
