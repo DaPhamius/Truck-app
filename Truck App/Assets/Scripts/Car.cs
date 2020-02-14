989a25893f7e009a901b9d7f0f21e7d83858531d
Dan Pham <43536392+DaPhamius@users.noreply.github.com>
2020-02-14 10:21:53 +0100

    Upload

---
+++ Truck App/Assets/Scripts/Car.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float acceleration;
+    public float maxSpeed;
+    public float turnSpeed;
+    [SerializeField]
+    private float curSpeed;
+    public float reverseAcceleration;
+    public float reverseMaxSpeed;
+    private float curReverseSpeed;
+    private float moveSpeed;
+
+    public bool doAccelerate;
+    public bool doReverse;
+
+    private Rigidbody rig;
+
+    // instance
+    public static Car instance;
+
+    private void Awake()
+    {
+        instance = this;
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Accelerate();
+    }
+
+    public void Turn(float rate)
+    {
+        transform.Rotate(Vector3.up, rate * turnSpeed * Time.deltaTime);
+    }
+
+    public void Accelerate()
+    {
+
+        if (doAccelerate)
+        {
+            curSpeed = Mathf.Clamp(curSpeed + (Time.deltaTime * acceleration), 0.0f, maxSpeed);
+            rig.velocity = transform.forward * curSpeed;
+        }
+
+        else if (doReverse)
+        {
+            curReverseSpeed = Mathf.Clamp(curReverseSpeed + (Time.deltaTime * reverseAcceleration), 0.0f, reverseMaxSpeed);
+            rig.velocity = -transform.forward * curReverseSpeed;
+        }
+
+        else
+        {
+            if (curSpeed > 0)
+            {
+                curSpeed = Mathf.Clamp(curSpeed - (Time.deltaTime * acceleration), 0.0f, maxSpeed);
+                rig.velocity = transform.forward * curSpeed;
+            }
+
+            if (curReverseSpeed > 0)
+            {
+                curReverseSpeed = Mathf.Clamp(curReverseSpeed - (Time.deltaTime * acceleration), 0.0f, maxSpeed);
+                rig.velocity = -transform.forward * curReverseSpeed;
+            }
+        }
+    }
+}
