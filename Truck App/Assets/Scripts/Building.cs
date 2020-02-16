45641c0569d4a4e8b1a5c6619b5d70854a658393
Dan Pham <43536392+DaPhamius@users.noreply.github.com>
2020-02-16 20:23:59 +0100

    Last finishing touches
    Last finishing touches with Added shadow to building and correct collision detection

---
+++ Truck App/Assets/Scripts/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+
+    public static Building instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Truck App/Assets/Scripts/ColliderDebug.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDebug : MonoBehaviour
+{
+    public GameObject hit;
+
+    private void Start()
+    {
+        hit.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        hit.SetActive(true);
+
+    }
+}
