4fba7bf1f052abb00d39570a3e1d41e4286820cf
Dan Pham <43536392+DaPhamius@users.noreply.github.com>
2020-02-17 14:37:34 +0100

    Final Update
    Compability for Android 7.0 Nougat or higher

---
+++ Truck Android/Assets/Scripts/AccelerateButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerateButton : MonoBehaviour
+{
+
+
+    public void OnDown()
+    {
+        Car.instance.doAccelerate = true;
+    }
+
+    public void OnUp()
+    {
+        Car.instance.doAccelerate = false;
+    }
+
+    public void ReverseDown()
+    {
+        Car.instance.doReverse = true;
+    }
+
+    public void ReverseUp()
+    {
+        Car.instance.doReverse = false;
+    }
+}
---
+++ Truck Android/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+
+    public GameObject car;
+    public GameObject placementIndicator;
+    public GameObject carControls;
+    public GameObject placeCarButton;
+    public GameObject building;
+
+    private void Start()
+    {
+        car.SetActive(false);
+        carControls.SetActive(false);
+        placeCarButton.SetActive(true);
+        placementIndicator.SetActive(true);
+        building.SetActive(false);
+    }
+
+    public void OnPlaceCarButton()
+    {
+        car.SetActive(true);
+        car.transform.position = placementIndicator.transform.position;
+        building.SetActive(true);
+        building.transform.position = placementIndicator.transform.position;
+        carControls.SetActive(true);
+        placeCarButton.SetActive(false);
+        placementIndicator.SetActive(false);
+    }
+}
